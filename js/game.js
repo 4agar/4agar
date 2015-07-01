48a410c4e928c579b05679ac79da508388db12e6
HappyToast <>
2015-07-01 13:30:15 +0200

    Added resizing canvas

--- js/game.js
+++ js/game.js
@@ -2,8 +2,18 @@ var canvas = document.getElementById('Scene');
 var context = canvas.getContext('2d');
 var FPS = 60;
 
+function update() {
+    
+}
+
+function draw() {
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+    //drawing code
+}
 
 // game loop
 setInterval(function() {
-   
+    update();
+    draw();
 }, 1000 / FPS);
