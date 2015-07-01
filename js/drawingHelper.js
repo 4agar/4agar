13636ef40eb30f531a7dad181deb530a4a27343f
HappyToast <>
2015-07-01 14:22:29 +0200

    Added drawing helper

---
+++ js/drawingHelper.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById('Scene');
+var context = canvas.getContext('2d');
+
+var Circle = function (x, y, radius, color) {
+    this.centerX = x / 2;
+    this.centerY = y / 2;
+    this.radius = radius;
+    this.color = color;
+};
+
+function drawCircle(centerX, centerY, radius, color, strokeColor) {
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = 5;
+    context.StrokeStyle = strokeColor;
+    context.stroke();
+}
