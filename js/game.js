a4d2f3d48fb9f8fdb55e3a3d0e739ce66f94e886
unknown <procasher@onet.pl>
2015-07-01 11:04:43 +0200

    Added some javascript code

--- js/game.js
+++ js/game.js
@@ -1,2 +1,9 @@
 var canvas = document.getElementById('Scene');
 var context = canvas.getContext('2d');
+var FPS = 60;
+
+
+//game loop
+setInterval(function() {
+   
+}, 1000 / FPS); // 1000 is an one second
