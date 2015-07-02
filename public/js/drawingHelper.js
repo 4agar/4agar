66bf974a386a2b8f6f29b8691ea6d6ed6b6da226
HappyToast <>
2015-07-02 22:14:06 +0200

    Updated drawing circle.

--- public/js/drawingHelper.js
+++ public/js/drawingHelper.js
@@ -1,10 +1,13 @@
-var Circle = function (x, y, radius, color) {
+var Circle = function (id, x, y, radius, color, strokeColor) {
+    this.id = id;
     this.centerX = x / 2;
     this.centerY = y / 2;
     this.radius = radius;
     this.color = color;
-    
-    this.drawCircle = function (centerX, centerY, radius, color, strokeColor) {
+    this.strokeColor = strokeColor;
+};
+
+function drawCircle (centerX, centerY, radius, color, strokeColor) {
     context.beginPath();
     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     context.fillStyle = color;
@@ -12,5 +15,4 @@ var Circle = function (x, y, radius, color) {
     context.lineWidth = 5;
     context.StrokeStyle = strokeColor;
     context.stroke();
-    }
-};
+}
